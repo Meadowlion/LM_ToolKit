e175dac13de7668b5b8fd4461da288ee2ee4feb9
Laith Mohammad <49846903+Meadowlion@users.noreply.github.com>
2021-06-30 13:23:16 -0600

    Add files via upload

---
+++ LMToolkit.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 14 17:08:19 2021
+
+@author: sb069
+"""
+
+import errno
+import os 
+#Guppy Start 
+
+#basecall=input("Drag and Drop the guppy_basecaller you wish to use here (should be found in /ont-guppy/bin/):  ")
+#basecall=basecall.strip()
+#basecall=basecall[:-1]
+#basecallconfig=input("Drag and Drop the guppy_basecaller you wish to use here (should be found in /ont-guppy/data/):  ")
+with open("config.txt","r") as basecallpath:
+    basecall=basecallpath.read()
+    print("This is your basecaller path " + basecall)
+with open("guppyconfig.txt","r") as configpath:
+    basecallconfig=configpath.read()
+    print("This is your config file path " + basecallconfig)    
+qinputgup=input("What is the name of the input fast5 folder (drag and drop it here): ")
+folderoutgup=input("What would you like to the name the output folder?:  ")
+q1=input("Are you expecting plasmids? [y/N]:  ")
+
+
+print("The output of guppy will be found here: MinionOut/"+folderoutgup)
+outpathgup="MinionOut/"+folderoutgup
+
+outpathgupfinal=outpathgup
+try:
+	os.system("mkdir " + outpathgup)
+except OSError as e:
+	if e.errno != errno.EEXIST:
+		raise
+string1='sudo  ' + basecall + '  -i ' + qinputgup +  ' -s ' + outpathgupfinal +  ' -c ' + basecallconfig + ' -x ' '"cuda:0"'
+os.system(string1)
+
+#Merger Start
+
+folderin=outpathgupfinal
+folderin.strip()
+#folderin=folderin[:-1]
+#folderin=folderin[1:]
+print(folderin) #check 1 for strip
+outfilename=folderoutgup
+outfilename=outfilename +'.fastq'
+print("the merged file will be located in " + folderin + " named as " + outfilename)
+os.chdir(folderin) #Check 2 for strip 
+
+os.system('cat ' '*.fastq ' '>' +outfilename)
+
+#Flye Start
+
+
+qinput=outfilename
+
+if q1=='y':
+    plasmid= '--plasmid '
+elif q1=='N':
+    plasmid=" "
+else:
+    q1
+os.chdir(os.path.expanduser("~"))
+print("The assembled output will be found here: Documents/LMToolkit/Assembly_Out/"+folderoutgup)
+outpath="Assembly_Out/"+folderoutgup
+outpath1="'"+outpath+"'"
+try:
+	os.system("mkdir -p " +outpath )
+except OSError as e:
+	if e.errno != errno.EEXIST:
+		raise
+inassemble=outpathgupfinal+"/"+outfilename
+print(inassemble)
+string2='flye ' '-o ' + outpath1 + ' --threads 14 ' '-i 6 ' ' --nano-raw ' + inassemble + " " + plasmid 
+print(string2)
+os.system(string2)
+print("This program has now finished!") 
---
+++ LMToolkit_Setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 22 10:19:44 2021
+
+@author: sb069
+"""
+import os
+import errno
+from os.path import expanduser
+os.system("sudo apt-get install python 3.8")
+os.system("conda install flye")
+
+try:
+	os.system("mkdir -p ~/Documents/LMToolkit")
+except OSError as e:
+	if e.errno != errno.EEXIST:
+		raise
+try:
+	os.system("mkdir -p ~/Documents/LMToolkit/MinionOut")
+except OSError as e:
+	if e.errno != errno.EEXIST:
+		raise
+try:
+	os.system("mkdir -p ~/Documents/LMToolkit/Assembly_Out")
+except OSError as e:
+	if e.errno != errno.EEXIST:
+		raise
+Download=str(os.environ['_'])
+
+
+Downloads=Download.replace('/LMToolkit_Setup.py','')
+print("THIS IS A TEST " + Downloads)
+
+os.system("dir")
+Dir=Downloads+"/"
+print("mv LMToolKit.py ~/Documents/LMToolkit")
+try:
+	os.system("mv LMToolKit.py ~/Documents/LMToolkit/LMToolkit.py")
+except OSError as e:
+	if e.errno != errno.EEXIST:
+		raise
+try:
+	os.system("mv README.md ~/Documents/LMToolkit/README.md")
+except OSError as e:
+	if e.errno != errno.EEXIST:
+		raise
+try:
+	os.system("mv LMToolKit_License ~/Documents/LMToolkit/LMToolKit_License")
+except OSError as e:
+	if e.errno != errno.EEXIST:
+		raise
+try:
+	os.system("mv Flye_License ~/Documents/LMToolkit/Flye_License")
+except OSError as e:
+	if e.errno != errno.EEXIST:
+		raise
+def main():
+    hdir=expanduser('~')
+    os.chdir(hdir+'/Documents/LMToolkit')
+    with open("config.txt", 'w') as config1:
+        Basecall=input("What is the path of your guppy basecaller?:  ")
+        config1.write(Basecall)
+        config1.close()
+    with open("guppyconfig.txt", "w") as config2:
+        Basecallconfig=input("What is the path of your guppy basecaller config file (450_bps_fast.cfg recommended)?:  ")
+        config2.write(Basecallconfig)
+        config2.close()
+    print("LMToolKit is now setup! Happy Basecalling!")
+
+main() 
